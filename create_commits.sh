bf8b5237697ec17d43ffe26620668cad525aeb05
Yoshi Automation <yoshi-automation@google.com>
2021-02-16 10:55:08 +0000

    wip

--- create_commits.sh
+++ create_commits.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+git config --global user.name 'Yoshi Automation'
+git config --global user.email 'yoshi-automation@google.com'
+
 while read api;
 do
     name=`echo $api | cut -d '.' -f 1`
-    git config --global user.name 'Yoshi Automation'
-    git config --global user.email 'yoshi-automation@google.com'
-    (cd googleapiclient/discovery_cache/documents && git add $name.*.json)
-    (cd docs/dyn && git add $name_*)
-    commitmsg=`cat temp/$name.verbose`
-    git commit -m "$commitmsg"
-    git push
+    API_SUMMARY_PATH=temp/$name.verbose
+    if [ -f "$API_SUMMARY_PATH" ]; then
+        git reset
+        git add googleapiclient/discovery_cache/documents/$name.*.json)
+        git add docs/dyn/$name_*)
+        commitmsg=`cat $API_SUMMARY_PATH`
+        git commit -m "$commitmsg"
+    fi
 done < changed_files
 
