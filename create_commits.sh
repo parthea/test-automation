fddd094983c317d7b571773c6d929f65b45bb65b
Yoshi Automation <yoshi-automation@google.com>
2021-02-17 16:12:38 +0000

    wip

--- create_commits.sh
+++ create_commits.sh
@@ -7,10 +7,8 @@ do
     name=`echo $api | cut -d '.' -f 1`
     API_SUMMARY_PATH=temp/$name.verbose
     if [ -f "$API_SUMMARY_PATH" ]; then
-        echo "Creating commits for $name\n"
         git add 'googleapiclient/discovery_cache/documents/'$name'.*.json'
         git add 'docs/dyn/'$name'_*.html'
-        cat $API_SUMMARY_PATH
         commitmsg=`cat $API_SUMMARY_PATH`
         git commit 'googleapiclient/discovery_cache/documents/'$name'.*.json' 'docs/dyn/'$name'_*.html' -m "$commitmsg"
     fi
